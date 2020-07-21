1ff9077b71b2bc9561071ba88ff815215a49233f
hide <padocon@naver.com>
2020-07-21 09:38:33 +0900

    Fix config.py

--- core/config.py
+++ core/config.py
@@ -8,26 +8,23 @@ class Config(BaseSettings):
     DEBUG: bool = True
     APP_HOST: str = '0.0.0.0'
     APP_PORT: int = 8000
-    DB_USER: str = 'fastapi'
-    DB_PASS: str = 'fastapi'
-    DB_HOST: str = 'localhost'
-    DB_NAME: str = 'fastapi'
-    DB_URL: str = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}'
+    DB_URL: str = f'mysql+pymysql://fastapi:fastapi@localhost:3306/fastapi'
     JWT_SECRET_KEY: str = 'fastapi'
     JWT_ALGORITHM: str = 'HS256'
     SENTRY_SDN: str = None
 
 
 class DevelopmentConfig(Config):
-    DEBUG: str = True
+    DB_URL: str = f'mysql+pymysql://fastapi:fastapi@localhost:3306/dev'
 
 
 class TestingConfig(Config):
-    DEBUG: str = True
+    DB_URL: str = f'mysql+pymysql://fastapi:fastapi@localhost:3306/test'
 
 
 class ProductionConfig(Config):
     DEBUG: str = False
+    DB_URL: str = f'mysql+pymysql://fastapi:fastapi@localhost:3306/prod'
 
 
 def get_config():
