c53a56abe846841b16375ff79a13131332a8288d
hide <padocon@naver.com>
2021-02-16 12:06:24 +0900

    Change PermissionDependency inherit SecurityBase for openapi

--- core/fastapi/dependencies/permission.py
+++ core/fastapi/dependencies/permission.py
@@ -1,14 +1,20 @@
 from typing import List
 
 from fastapi import Request
+from fastapi.openapi.models import APIKey, APIKeyIn
+from fastapi.security.base import SecurityBase
 
 from app.usecases import UserUsecase
 from core.exceptions import UnauthorizedException
 
 
-class PermissionDependency:
+class PermissionDependency(SecurityBase):
     def __init__(self, permissions: List):
         self.permissions = permissions
+        self.model: APIKey = APIKey(
+            **{"in": APIKeyIn.header}, name="Authorization"
+        )
+        self.scheme_name = self.__class__.__name__
 
     async def __call__(self, request: Request):
         for permission in self.permissions:
