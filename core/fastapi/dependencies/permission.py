ea92cce7f3801dacae6e09ad09d686dd057bd36b
Hide <padocon@naver.com>
2022-05-03 23:18:37 +0900

    Add typing to permissions

--- core/fastapi/dependencies/permission.py
+++ core/fastapi/dependencies/permission.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Type
 
 from fastapi import Request
 from fastapi.openapi.models import APIKey, APIKeyIn
@@ -7,19 +7,6 @@ from fastapi.security.base import SecurityBase
 
 from app.user.services import UserService
 from core.exceptions import CustomException, UnauthorizedException
-
-
-class PermissionDependency(SecurityBase):
-    def __init__(self, permissions: List):
-        self.permissions = permissions
-        self.model: APIKey = APIKey(**{"in": APIKeyIn.header}, name="Authorization")
-        self.scheme_name = self.__class__.__name__
-
-    async def __call__(self, request: Request):
-        for permission in self.permissions:
-            cls = permission()
-            if not await cls.has_permission(request=request):
-                raise cls.exception
 
 
 class BasePermission(ABC):
@@ -51,3 +38,16 @@ class IsAdmin(BasePermission):
 class AllowAll(BasePermission):
     async def has_permission(self, request: Request) -> bool:
         return True
+
+
+class PermissionDependency(SecurityBase):
+    def __init__(self, permissions: List[Type[BasePermission]]):
+        self.permissions = permissions
+        self.model: APIKey = APIKey(**{"in": APIKeyIn.header}, name="Authorization")
+        self.scheme_name = self.__class__.__name__
+
+    async def __call__(self, request: Request):
+        for permission in self.permissions:
+            cls = permission()
+            if not await cls.has_permission(request=request):
+                raise cls.exception
