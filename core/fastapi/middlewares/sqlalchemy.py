d532a064506090a30764e31821e8067fc904a1e4
hide <padocon@naver.com>
2022-05-31 09:19:37 +0900

    Replace SQLAlchemyMiddleware inherit BaseHTTPMiddleware to Raw ASGI Middleware

--- core/fastapi/middlewares/sqlalchemy.py
+++ core/fastapi/middlewares/sqlalchemy.py
@@ -1,30 +1,22 @@
 from uuid import uuid4
 
-from fastapi import Request
-from starlette.middleware.base import (
-    BaseHTTPMiddleware,
-    RequestResponseEndpoint,
-)
+from starlette.types import ASGIApp, Receive, Scope, Send
 
 from core.db.session import set_session_context, reset_session_context, session
 
 
-class SQLAlchemyMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app):
-        super().__init__(app)
+class SQLAlchemyMiddleware:
+    def __init__(self, app: ASGIApp) -> None:
+        self.app = app
 
-    async def dispatch(
-        self, request: Request, call_next: RequestResponseEndpoint,
-    ):
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         session_id = str(uuid4())
         context = set_session_context(session_id=session_id)
 
         try:
-            response = await call_next(request)
+            await self.app(scope, receive, send)
         except Exception as e:
             raise e
         finally:
             await session.remove()
             reset_session_context(context=context)
-
-        return response
