71193effcb8646658e5e117c88fad92e449bcf0b
Hide <padocon@naver.com>
2022-08-30 22:08:52 +0900

    Install pytest/pytest-asyncio and add test templates

---
+++ tests/app/user/services/test_user.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_get_user_list():
+    ...
+
+
+@pytest.mark.asyncio
+async def test_create_user():
+    ...
+
+
+@pytest.mark.asyncio
+async def test_is_admin():
+    ...
