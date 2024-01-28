ad5cbf904bcbec9c4ae0fcf87a07deca46849877
Hide <padocon@naver.com>
2024-01-28 18:07:15 +0900

    Fix permission test codes

--- tests/core/fastapi/dependencies/test_permission.py
+++ tests/core/fastapi/dependencies/test_permission.py
@@ -1,17 +1,18 @@
-from unittest.mock import AsyncMock, Mock, patch
+from unittest.mock import AsyncMock, Mock
 
 import pytest
 from fastapi import Request
 
-from app.user.application.service.user import UserService
+from app.container import Container
 from core.fastapi.dependencies import (
-    PermissionDependency,
+    AllowAll,
     IsAdmin,
     IsAuthenticated,
-    AllowAll,
+    PermissionDependency,
 )
-from core.fastapi.dependencies import permission
 from core.fastapi.dependencies.permission import UnauthorizedException
+
+container = Container()
 
 
 @pytest.mark.asyncio
@@ -27,31 +28,34 @@ async def test_permission_dependency_is_authenticated():
 
 
 @pytest.mark.asyncio
-@patch.object(permission, "UserService", spec=UserService)
-async def test_permission_dependency_is_admin_user_is_not_admin(user_service_mock):
+async def test_permission_dependency_is_admin_user_is_not_admin():
     # Given
     dependency = PermissionDependency(permissions=[IsAdmin])
     request = AsyncMock(spec=Request)
     user_id = 1
     request.user = Mock(id=user_id)
+    user_service_mock = AsyncMock()
     user_service_mock.is_admin.return_value = False
 
     # When, Then
-    await dependency(request=request)
+    with container.user_service.override(user_service_mock):
+        with pytest.raises(UnauthorizedException):
+            await dependency(request=request)
 
 
 @pytest.mark.asyncio
-@patch.object(permission, "UserService", spec=UserService)
-async def test_permission_dependency_is_admin_user_id_is_none(user_service_mock):
+async def test_permission_dependency_is_admin_user_id_is_none():
     # Given
     dependency = PermissionDependency(permissions=[IsAdmin])
     request = AsyncMock(spec=Request)
     request.user = Mock(id=None)
+    user_service_mock = AsyncMock()
     user_service_mock.is_admin.return_value = False
 
     # When, Then
-    with pytest.raises(UnauthorizedException):
-        await dependency(request=request)
+    with container.user_service.override(user_service_mock):
+        with pytest.raises(UnauthorizedException):
+            await dependency(request=request)
 
 
 @pytest.mark.asyncio
